c8ad6f0470327a4c39625c82fae451a0d9c13213
omagdiel <omagdiel@gmail.com>
2018-03-27 14:03:19 -0600

    configured network setting for rails api

--- freelance_camp_documents/db/seeds.rb
+++ freelance_camp_documents/db/seeds.rb
@@ -1,4 +1,4 @@
-11.times do |d|
+10.times do |d|
   FreelanceDocument.create!(title: "Document #{d}",
     description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
     file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit",
