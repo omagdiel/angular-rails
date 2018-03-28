ca2b4ed11807f07749990804c117bd15c5ebef11
omagdiel <omagdiel@gmail.com>
2018-03-28 09:43:32 -0600

    linked the Rails documents to the Angular through api

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,36 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit{
   pageTitle: string = "Document Dashboard"
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
 
-  documents: Document[] = [
-    {
-      title: "My first doc",
-      description: "Stuff",
-      file_url: "http://google.com",
-      updated_at: '11/11/16',
-      image_url: "https://careerlancer.net/wp-content/uploads/2015/12/challenges-freelancer.jpg",
-    },
-    {
-      title: "My secod doc",
-      description: "Good Stuff",
-      file_url: "http://google.com",
-      updated_at: '11/11/16',
-      image_url: "https://careerlancer.net/wp-content/uploads/2015/12/challenges-freelancer.jpg",
-    },
-    {
-      title: "My last doc",
-      description: "Good Stuff",
-      file_url: "http://google.com",
-      updated_at: '11/11/16',
-      image_url: "https://careerlancer.net/wp-content/uploads/2015/12/challenges-freelancer.jpg",
-    }
-  ]
+  constructor(
+    private documentService: DocumentService,
+  ){}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
